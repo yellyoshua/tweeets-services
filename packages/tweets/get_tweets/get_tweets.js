e7c5e08c27d28ac84ee9c949aff2a85a49be54a9
yellyoshua <usuario1529yoshua@hotmail.com>
2022-08-21 12:17:21 -0500

    fix 2 logs inside request

--- packages/tweets/get_tweets/get_tweets.js
+++ packages/tweets/get_tweets/get_tweets.js
@@ -1,6 +1,6 @@
 function main(args) {
-  const args = Array.from(arguments);
-  console.log('data', JSON.stringify({args}, null, 2));
+  const argsList = Array.from(arguments);
+  console.log('data', JSON.stringify(argsList, null, 2));
 
   let name = args.name || 'stranger'
   let greeting = 'Hello ' + name + '!'
